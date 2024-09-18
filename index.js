034371f9d902f94805e5e3eafa56588d16704411
Dennis <dennismusembi344@gmail.com>
2024-09-18 11:09:12 +0300

    redone

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const divide=function (){
-    return 2000/100
+// const divide=function (){
+//     return 2000/100
 
-}
-const square= num1=>num1**num1
-square(2)
+// }
+// const square= num1=>num1**num1
+// square(2)
 
-const add=(a,b)=>a+b
-add(3,4)
+// const add=(a,b)=>a+b
+// add(3,4)
+let num1=100
+let num2=2000
+const divide=()=>{
+    return 2000/100}
+const square=(num1=>num1**num1)
+const add=((num1,num2)=>num1+num2)
